9a22138941461243fd50b58f8b170f963c4b618d
Andrade020 <lucas_r_andrade@hotmail.com>
2025-02-25 18:57:20 -0300

    uploading init

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .base_excel_processador import BaseExcelProcessor
+from .carteira_diaria_processor import CarteiraDiariaProcessor
+from .mapa_cotas_processor import MapaCotasProcessor
+from .extratos_processor import ExtratosProcessor
+
+__all__ = [
+    "BaseExcelProcessor",
+    "CarteiraDiariaProcessor",
+    "MapaCotasProcessor",
+    "ExtratosProcessor"
+]
